e63938cc68e3bb60cc74940e188c8c91553959db
Danielevaccari <daniele.j.vaccari@gmail.com>
2026-01-18 16:36:15 +0200

    Remove burger icon from navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,8 +3,6 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import { EThemeMode, IThemeContext, ThemeContext } from "./ThemeProvider";
 import { Switch } from "@mui/material";
 
@@ -19,15 +17,6 @@ const NavigationBar = () => {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" style={{ background: backgroundColor }}>
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ marginRight: 2 }}>
-                        <MenuIcon />
-                    </IconButton>
-
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
                     <Switch
                         checked={theme.mode === EThemeMode.Light}
